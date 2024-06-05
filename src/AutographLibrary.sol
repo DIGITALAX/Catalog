42b98bdc469c83cce8104e723d32866a4e6786ea
emmajane1313 <admin@digitalax.xyz>
2024-06-05 11:40:29 -0400

    base

--- src/AutographLibrary.sol
+++ src/AutographLibrary.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.26;
 contract AutographLibrary {
     enum AutographType {
         CollectionNFT,
-        CollectionPrint,
+        CollectionHoodie,
+        CollectionShirt,
         Catalog,
         Mix
     }
@@ -17,28 +18,30 @@ contract AutographLibrary {
 
     struct OpenActionParams {
         address[] acceptedTokens;
-        uint256[] prices;
         string uri;
-        uint256 amount;
         AutographType autographType;
         uint256 collectionId;
+        uint256 amount;
+        uint256 price;
         uint16 galleryId;
     }
 
     struct AutographInit {
         address[] acceptedTokens;
-        uint256[] prices;
         string uri;
+        address designer;
         uint256 amount;
+        uint256 price;
         uint256 pubId;
         uint256 profileId;
     }
 
     struct Autograph {
         address[] acceptedTokens;
-        uint256[] prices;
         string uri;
+        address designer;
         uint256 amount;
+        uint256 price;
         uint256 id;
         uint256 pubId;
         uint256 profileId;
@@ -46,7 +49,7 @@ contract AutographLibrary {
 
     struct CollectionInit {
         address[][] acceptedTokens;
-        uint256[][] prices;
+        uint256[] prices;
         string[] uris;
         uint256[] amounts;
         CollectionType[] collectionTypes;
@@ -54,12 +57,12 @@ contract AutographLibrary {
 
     struct Collection {
         address[] acceptedTokens;
-        uint256[] prices;
         uint256[] pubIds;
         uint256[] profileIds;
         uint256[] mintedTokenIds;
         string uri;
         address designer;
+        uint256 price;
         uint256 amount;
         uint256 galleryId;
         uint256 collectionId;
